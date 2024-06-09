24104eaa71477ebca03cb941a655cbd006b38c78
JacobPercy <jacob.percy@icloud.com>
2024-06-08 21:09:40 -0400

    added gyro

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-filename_1 = "accel_1.csv"
-filename_2 = "gyro_1.csv"
+filename_1 = "accel_2.csv"
+filename_2 = "gyro_2.csv"
+
 df_1 = pd.read_csv(filename_1, delimiter=";")
+df_2 = pd.read_csv(filename_2, delimiter=";")
 
 data_1 = df_1[['AccelerationX', 'AccelerationY', 'AccelerationZ']].values
-
+data_2 = df_2[['GyroscopeX', 'GyroscopeY', 'GyroscopeZ']].values
 
 
 class Point():
@@ -29,11 +31,12 @@ x=y=z=0.0 #Initialize starting values of vector
 x_off,y_off,z_off = (-data_1[0][0], -data_1[0][1], -data_1[0][2]) #Values of offsets
 
 data_1 = data_1[1:]
+data_2 = data_2[1:]
 
-for val in data_1:
-    x,y,z = val[0]+x_off,val[1]+y_off,val[2]+z_off
+for ac,gy in zip(data_1,data_2):
+    x,y,z = ac[0]+x_off,ac[1]+y_off,ac[2]+z_off
 
-    
+
 
     p1.update(x,y,z)
     p1.locate()
